b9c0e5f1dc9a299809fafa5bd8d70e6c37003198
Antonin <antonin.radet@protonmail.com>
2021-05-22 09:48:31 +0200

    Add Ex9

--- Chapter1.scm
+++ Chapter1.scm
@@ -86,3 +86,31 @@
     (cube-root-iter (improve-cube guess x) x)))
 
 (define (cube-root x) (cube-root-iter 1.O x))
+
+; Exercise 9
+
+; With the first program
+; (+ 4 5)
+; (inc (+ 3 5))
+; (inc (inc (+ 2 5)))
+; (inc (inc (inc (+ 1 5))))
+; (inc (inc (inc (inc (+ 0 5)))))
+; (inc (inc (inc (inc 5))))
+; (inc (inc (inc 6)))
+; (inc (inc 7))
+; (inc 8)
+; 9
+; this is a recursive process
+
+; With the second program
+; (+ 4 5)
+; (+ (dec 4) (inc 5))
+; (+ 3 6)
+; (+ (dec 3) (inc 6))
+; (+ 2 7)
+; (+ (dec 2) (inc 7))
+; (+ 1 8)
+; (+ (dec 1) (inc 8))
+; (+ 0 9)
+; 9
+; this is an iterative process
