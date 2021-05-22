d6014967d0df108c36ca2e7f69ecf43e1b3bcea6
Antonin <antonin.radet@protonmail.com>
2021-05-22 10:04:36 +0200

    Add Ex10

--- Chapter1.scm
+++ Chapter1.scm
@@ -114,3 +114,29 @@
 ; (+ 0 9)
 ; 9
 ; this is an iterative process
+
+; Exercise 10
+
+(define (A x y) (cond ((= y 0) 0) ((= x 0) (* 2 y))
+((= y 1) 2) (else (A (- x 1) (A x (- y 1))))))
+
+; (A 1 10)
+; (A (- 1 1) (A 1 (- 10 1)))
+; (A 0 (A 1 9))
+; (* 2 (A 1 9))
+; (* 2 (A (- 1 1) (A 1 (- 9 1))))
+; (* 2 (A 0 (A 1 8)))
+; (* 2 (* 2 (A 1 8)))
+; ...
+; 1024
+
+; (A 2 4)
+; 65536
+
+; (A 3 3)
+; 65536
+
+; (f n) computes 2*n
+; (g n) computes 2^n
+; (h n) computes (A 2 n) = (A 1 (A 2 (n-1))) = 2^(A 2 (n-1)) so (h n) = 2^2^...^2 (n times)
+; (k n) computes 5n^2
